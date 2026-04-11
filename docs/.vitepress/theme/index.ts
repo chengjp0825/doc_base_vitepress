33cf145a34c6c22bbfb7e40b93866beab36ace30
chengjp <chengjp0825@163.com>
2026-04-11 12:23:08 +0800

    fix: 修复 medium-zoom SSR 构建失败问题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,17 @@
 // docs/.vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
-import mediumZoom from 'medium-zoom'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    mediumZoom('img', {
-      margin: 24,
-      background: 'rgba(0,0,0,0.85)'
-    })
+    if (!import.meta.env.SSR) {
+      import('medium-zoom').then((mediumZoom) => {
+        mediumZoom.default('img', {
+          margin: 24,
+          background: 'rgba(0,0,0,0.85)'
+        })
+      })
+    }
   }
 }
